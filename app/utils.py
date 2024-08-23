0e78914b8901a12e1da4012c5a6664cf39e90c13
Bisrat Kebere <kiyyakebere@gmail.com>
2024-08-23 04:42:01 +0300

    feat: data cleaning function

---
+++ app/utils.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+def clean_data(data):
+    data = data.replace([np.inf, -np.inf], np.nan)
+
+    if 'Timestamp' in data.columns:
+        data['Timestamp'] = pd.to_datetime(data['Timestamp'], errors='coerce')
+        most_common_date = (
+            data['Timestamp'].mode()[0] if not data['Timestamp'].mode().empty else pd.Timestamp.now()
+        )
+        data['Timestamp'] = data['Timestamp'].fillna(most_common_date)
+        data.set_index('Timestamp', drop=False, inplace=True)
+
+    numeric_columns = data.select_dtypes(include=[np.number]).columns
+    data[numeric_columns] = data[numeric_columns].apply(lambda x: x.fillna(x.median()))
+
+    data.drop_duplicates(inplace=True)
+
+    return data
+
+def get_summary_stats(data):
+    return data.describe()
